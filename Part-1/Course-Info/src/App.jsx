bc63bf3881b898fa80a1e5e1fff59a1aff715c09
Rishav Basnet <jaden.smith4884@gmail.com>
2026-05-01 05:11:50 -0400

    Complete Part-1b Exercise-1.5

--- Part-1/Course-Info/src/App.jsx
+++ Part-1/Course-Info/src/App.jsx
@@ -1,42 +1,41 @@
 const App = () => {
-  const course = "Half Stack Application Development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack Application Development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
 
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return <h1>{props.course.name}</h1>;
 };
 
 const Content = (props) => {
   return (
     <>
-      <Part part1={props.part1} exercises1={props.exercises1} />
-      <Part part2={props.part2} exercises2={props.exercises2} />
-      <Part part3={props.part3} exercises3={props.exercises3} />
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
     </>
   );
 };
@@ -44,32 +43,18 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <p>
-      Total Number of exercises{" "}
-      {props.exercises1 + props.exercises2 + props.exercises3}
+      Total Number of exercises:{" "}
+      {props.course.parts[0].exercises +
+        props.course.parts[1].exercises +
+        props.course.parts[2].exercises}
     </p>
   );
 };
 
 const Part = (props) => {
-  let partName;
-  let exerciseCount;
-
-  if (Object.hasOwn(props, "exercises1")) {
-    console.log("Entered Part 1");
-    partName = props.part1;
-    exerciseCount = props.exercises1;
-  } else if (Object.hasOwn(props, "exercises2")) {
-    console.log("Entered Part 2");
-    partName = props.part2;
-    exerciseCount = props.exercises2;
-  } else if (Object.hasOwn(props, "exercises3")) {
-    console.log("Entered Part 3");
-    partName = props.part3;
-    exerciseCount = props.exercises3;
-  }
   return (
     <p>
-      {partName} {exerciseCount} exercises
+      {props.part.name}: {props.part.exercises} exercises
     </p>
   );
 };
