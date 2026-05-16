40c8b11e2a0ece15e0076ec2bc6f4e31c0550cbb
Rishav Basnet <jaden.smith4884@gmail.com>
2026-05-16 03:36:39 -0400

    DONE EX 2.11: Phonebook Step 6

--- Part-2/Phonebook/src/App.jsx
+++ Part-2/Phonebook/src/App.jsx
@@ -1,21 +1,27 @@
-import { useState } from "react";
-import Persons from "./components/Persons"
+import { useState, useEffect } from "react";
+import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
-
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [personsToShow, setPersonsToShow] = useState([]);
   const [newFilter, setNewFilter] = useState("");
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      const fetchedPersons = response.data
+      console.log(fetchedPersons)
+      setPersons(fetchedPersons)
+  })
+
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -55,7 +61,13 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <Filter newFilter={newFilter} handleFilter={handleFilter} />
-      <PersonForm newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} handleSubmit={handleSubmit} /> 
+      <PersonForm
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+        handleSubmit={handleSubmit}
+      />
       <h2>Numbers</h2>
       <Persons
         persons={persons}
@@ -65,6 +77,5 @@ const App = () => {
     </div>
   );
 };
-
 
 export default App;
