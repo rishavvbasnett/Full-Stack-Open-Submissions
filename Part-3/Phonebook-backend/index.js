210aebb5dad902f2cd39251eba468665500f524a
Rishav Basnet <jaden.smith4884@gmail.com>
2026-05-25 02:40:32 -0400

    DONE Ex-3.6: Phonebook Backend Step 6

--- Part-3/Phonebook-backend/index.js
+++ Part-3/Phonebook-backend/index.js
@@ -1,7 +1,25 @@
 import express, { json, request, response } from "express";
-import numbersList from "./data.json" with { type: "json" };
+
+let numbersList = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+];
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send("<h1>Welcome to my server</h1>");
@@ -11,25 +29,56 @@ app.get("/api/persons", (request, response) => {
   response.json(numbersList);
 });
 
-app.get('/info' , (request, response) => {
+app.get("/info", (request, response) => {
   response.send(`
     <p>Phonebook has info for ${numbersList.length} entries </p>
     <p>${new Date(Date.now()).toString()}</p>
-    `)
-})
+    `);
+});
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const requestedPerson = numbersList.find(person => person.id === id)
-  
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const requestedPerson = numbersList.find((person) => person.id === id);
+
   if (requestedPerson) {
-    response.json(requestedPerson)
+    response.json(requestedPerson);
   } else {
-    response.status(404).json({"error": "Id doesn't exist"})
+    response.status(404).json({ error: "Person not found!" });
   }
+});
 
-})
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
 
+  const requestedPerson = numbersList.find((person) => person.id === id);
 
+  if (requestedPerson) {
+    numbersList = numbersList.filter((person) => person.id !== id);
+    response.send(`<p>${requestedPerson.name} successfully deleted!</p>`);
+  } else {
+    response.status(404).json({ error: "Person not found!" });
+  }
+});
+
+app.post("/api/persons/", (request, response) => {
+  const requestedPerson = request.body;
+
+  if (!requestedPerson.name || !requestedPerson.number) {
+    response.status(400).json({ error: "Name or Number is missing!" });
+  } else if (
+    numbersList.find((person) => person.name === requestedPerson.name)
+  ) {
+    response.status(400).json({ error: "Name already exists!" });
+  } else if (
+    numbersList.find((person) => person.number === requestedPerson.number)
+  ) {
+    response.status(400).json({ error: "Number already exists!" });
+  } else {
+    requestedPerson.id = generateId();
+    response.json(requestedPerson);
+  }
+});
 
 app.listen(3001);
+
+const generateId = () => Math.floor(Math.random() * 100000);
