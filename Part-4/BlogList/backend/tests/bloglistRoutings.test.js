ef4c57535d238e7e49595780956392edb31b0cd6
rishavvbasnett <your-noreply-email>
2026-06-17 03:48:16 -0400

    DONE EX 4.14: Blog List Expansions Step 4

--- Part-4/BlogList/backend/tests/bloglistRoutings.test.js
+++ Part-4/BlogList/backend/tests/bloglistRoutings.test.js
@@ -6,6 +6,8 @@ import Blog from "../models/blog.js";
 import { info, error } from "../utils/logger.js";
 import { promiseHooks } from "node:v8";
 import mongoose from "mongoose";
+import { title } from "node:process";
+import { response } from "express";
 
 const api = supertest(app);
 
@@ -75,6 +77,37 @@ test("post increases total blogs count by 1", async () => {
   const allBlogs = await getResponse.body;
 
   assert.strictEqual(allBlogs.length, fourBlogs.length + 1);
+});
+
+test("patches the first bloglist properly", async () => {
+  const initialGetResponse = await api.get("/api/blogs");
+  const oldlist = initialGetResponse.body;
+  const firstId = oldlist[0].id;
+
+  const patchResponse = await api
+    .patch(`/api/blogs/${firstId}`)
+    .send({
+      title: "UPDATED: " + oldlist[0].title,
+    })
+    .expect("Content-Type", /application\/json/);
+
+  const finalGetResponse = await api.get("/api/blogs");
+  const newlist = finalGetResponse.body;
+
+  assert.ok(oldlist[0].title !== newlist[0].title);
+});
+
+test.only("delete successfully removes a blog", async () => {
+  const response = await api.get("/api/blogs");
+  const oldList = response.body;
+  const id = oldList[0];
+
+  await api.delete(`/api/notes/${id}`);
+
+  const newResponse = await api.get("/api/blogs");
+  const newList = newResponse.body;
+
+  assert(newList.length, fourBlogs.length - 1);
 });
 
 const saveBlog = async (blog) => {
